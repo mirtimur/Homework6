91843140bc3a2b21019ec95dcc404d226ee9ddac
Mir TImur <mirtimur1@gmail.com>
2023-12-15 14:31:10 +0300

    Homework

--- TMSHomework6Mir/TMSHomework6Mir/ViewController.swift
+++ TMSHomework6Mir/TMSHomework6Mir/ViewController.swift
@@ -1,19 +1,28 @@
-//
-//  ViewController.swift
-//  TMSHomework6Mir
-//
-//  Created by Timur Mir on 13.12.23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let student = Student(id: 0, name: "Biba", lastName: "Boba", age: 18)
+    let teacher = Teacher(id: 1, firstName: "Mikle", lastName: "Miklovich")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+        addSubject(.init(subjectName: "Math", grade: "4"))
+        addTeacher(teacher)
+        print(student.studentInfo(subject: "Math"))
+        teacher.studentGrading(subject: .init(subjectName: "Biology", grade: "6") , student: student)
+        print(student.studentInfo(subject: "Biology"))
+        print(teacher.teacherInfo(studentId: student.studentId))
     }
-
-
+    
+    private func addSubject(_ subject: Subject) {
+        student.addSubject(subject)
+        teacher.addSubject(subject)
+    }
+    
+    private func addTeacher(_ teacher: Teacher) {
+        student.addTeacherToStudent(teacher)
+        teacher.addStudent(student)
+    }
 }
-
